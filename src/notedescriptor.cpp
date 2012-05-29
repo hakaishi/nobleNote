0def00a04e802982f4eb322d4812890ebaecb5e1
Taiko <taiko000@gmail.com>
2012-05-29 14:00:26 +0200

    minor updates

--- src/notedescriptor.cpp
+++ src/notedescriptor.cpp
@@ -9,19 +9,24 @@ NoteDescriptor::NoteDescriptor(QString filePath, TextDocument *document, QObject
     stateChangeEnabled = false; // this will block focusInEvent from the textEdit if this object is constructed
     filePath_ = filePath;
     document_ = document;
+
     XmlNoteReader reader(filePath);
     reader.setFrame(document->rootFrame());
-    reader.read();
+    reader.read(); // add the read text to the document
+
     uuid_ = reader.uuid();
     if(uuid_.isNull())
     {
-        // should ask to save the file in this program's format with a generated uuid
+        // should overwrite the file in this program's format with a generated uuid
         qDebug("NoteDescriptor::NoteDescriptor : uuid is null, TODO missing implementation of uuid generation");
     }
 
     lastChange_ = reader.lastChange();
-
-
+    if(lastChange_.isNull())
+    {
+        // should overwrite the file in this program's format with the current last change date
+        qDebug("NoteDescriptor::NoteDescriptor : uuid is null, TODO missing implementation of uuid generation");
+    }
 
     document_->setModified(false); // avoid emit of delayedModificationChanged()
     connect(document_,SIGNAL(delayedModificationChanged()),this,SLOT(stateChange()));
@@ -41,13 +46,14 @@ void NoteDescriptor::stateChange()
 
     if(newFilePath.isEmpty())
     {
-        // TODO handle missing file
+        // TODO handle missing file, old file path is in filePath_
         return;
     }
+    filePath_ = newFilePath; // old filePath_ not longer needed
 
     XmlNoteReader reader(newFilePath);
     reader.read();
-    QDateTime laterLastChange = reader.lastChange();
+    QDateTime laterLastChange = reader.lastChange(); // TODO handle last change is null
 
     if(this->lastChange_ < laterLastChange)
     {
@@ -67,8 +73,6 @@ void NoteDescriptor::stateChange()
         document_->setModified(false);
         return;
     }
-
-    qDebug(newFilePath.toAscii());
 }
 
 void NoteDescriptor::setStateChangeEnabled()
