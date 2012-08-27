538efed1ee7ca7a2b318f0bd961f68ce5adc04bf
Christian Metscher <hakaishi@web.de>
2012-08-27 11:50:41 +0200

    fixed bug #95

--- src/note.cpp
+++ src/note.cpp
@@ -43,6 +43,8 @@ Note::Note(QString filePath, QWidget *parent) : QMainWindow(parent){
      setupUi(this);
      setAttribute(Qt::WA_DeleteOnClose);
 
+     setFont(QFont("DejaVu Sans", 10)); //setting standard for initializing
+
      searchbarVisible = false; //initializing
 
      textBrowser = new TextBrowser(this);
