4c3ceeef4a3d3daff5b3869a272dcb8857a16116
Taiko T <taiko000@gmail.com>
2012-11-29 23:02:19 +0100

    minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 #include <QLibraryInfo>
 #include <QSettings>
 #include <QMessageBox>
-#include <welcome.h>
+#include "welcome.h"
 
 int main (int argc, char *argv[]){
      QApplication app(argc, argv);
